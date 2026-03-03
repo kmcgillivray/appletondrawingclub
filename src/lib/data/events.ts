e778c58a6cf55e4817153221649e16c86d98df76
Kevin McGillivray <kevin.mcgillivray@me.com>
2026-03-03 17:09:01 -0600

    content: update events

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -408,8 +408,9 @@ export const eventsData: { events: Event[] } = {
     },
     {
       id: '2026-04-08-figure-drawing',
+      url: '/events/2026-04-08-figure-drawing',
       image_url: 'https://res.cloudinary.com/db5mnmxzn/image/upload/c_fill,g_center,h_750,w_750/v1730471148/IMG_0191_afzi3o.jpg',
-      title: 'Coming soon',
+      title: 'Mixed Pose Figure Drawing',
       date: '2026-04-08',
       time: "7:00-9:00PM",
       location: {
@@ -421,12 +422,12 @@ export const eventsData: { events: Event[] } = {
           postalCode: "54911",
         },
       },
-      model: '',
+      model: 'Erick',
       summary: '',
       description: '',
       price: 15,
       event_type: 'figure_drawing',
-      status: 'coming_soon',
+      status: 'registration_open',
     },
     {
       id: '2026-04-15-portrait-club',
