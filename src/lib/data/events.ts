ef02fc49ad3247db4440b2f98cd0feae308e754f
Kevin McGillivray <kevin.mcgillivray@me.com>
2026-06-20 20:03:02 -0500

    content: update image

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -780,7 +780,7 @@ export const eventsData: { events: Event[] } = {
     {
       id: "2026-07-15-figure-drawing",
       image_url:
-        "https://res.cloudinary.com/db5mnmxzn/image/upload/c_fill,g_center,h_750,w_750/v1757944086/IMG_7620_1_dt4d1g.jpg",
+        "https://res.cloudinary.com/db5mnmxzn/image/upload/c_fill,g_center,h_750,w_750/v1730471148/IMG_0191_afzi3o.jpg",
       url: "/events/2026-07-15-figure-drawing",
       title: "Mixed Pose Figure Drawing",
       date: "2026-07-15",
